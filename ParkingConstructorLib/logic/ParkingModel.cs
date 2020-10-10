8bc46952b1b0ec1a688a6ea4eb59e2928fd10adc
John Pier <warhamer10tw@gmail.com>
2020-10-10 19:10:49 +0400

    update logic

--- ParkingConstructorLib/logic/ParkingModel.cs
+++ ParkingConstructorLib/logic/ParkingModel.cs
@@ -10,17 +10,21 @@ namespace ParkingConstructorLib.logic
     public class ParkingModel
     {
         private ParkingModelElement[,] parkingLot;
+        private int columnCount;
+        private int rowColumn;
 
         public ParkingModel(int columnCount, int rowColumn)
         {
-            this.parkingLot = new ParkingModelElement[columnCount, rowColumn];
+            this.columnCount = columnCount;
+            this.rowColumn = rowColumn; 
+            parkingLot = new ParkingModelElement[columnCount, rowColumn];
         }
 
         public void SetElement(int columnIndex, int rowIndex, ParkingModelElement element)
         {
             try
             {
-                this.parkingLot[columnIndex, rowIndex] = element;
+                parkingLot[columnIndex, rowIndex] = element;
             }
             catch (Exception e)
             {
@@ -31,6 +35,18 @@ namespace ParkingConstructorLib.logic
         public bool IsParkingModelCorrect()
         {
             return true;
+        }
+
+        public void Clear()
+        {
+            this.Clear(this.columnCount, this.rowColumn);
+        }
+
+        public void Clear(int columnCount, int rowColumn)
+        {
+            this.columnCount = columnCount;
+            this.rowColumn = rowColumn;
+            parkingLot = new ParkingModelElement[columnCount, rowColumn];
         }
     }
 }
